af835ebc76ebcab72fc7d018fe73d16f5c0e50f1
Valeriano A.R <valen.alfonso@gmail.com>
2018-12-03 14:33:12 +0100

    Day01: Refactor resolving code

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -8,13 +8,13 @@ namespace AdventOfCode2018
         private static void Main(string[] args)
         {
 
-            // Resolve Day01 input data
+            // Resolve Day01
             Day01 day01 = new Day01();
-            string[] lines = File.ReadAllLines("inputs/Day01.txt");
-            string result = day01.ResolveDay01(lines);
-            Console.WriteLine("Day01 Result: {0}", result);
-            string resultPart2 = day01.ResolveDay01_Part2(lines);
-            Console.WriteLine("Day01_Part2 Result: {0}", resultPart2);
+            string[] linesDay01 = File.ReadAllLines("inputs/Day01.txt");
+            string resultDay01 = day01.ResolveDay01(linesDay01);
+            Console.WriteLine("Day01 Result: {0}", resultDay01);
+            string resultDay01Part2 = day01.ResolveDay01_Part2(linesDay01);
+            Console.WriteLine("Day01_Part2 Result: {0}", resultDay01Part2);
 
             Console.Read();
         }
