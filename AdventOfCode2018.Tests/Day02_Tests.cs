fdef1e7356594a0309e92f8975f2fbb05bf98270
Valeriano A.R <valen.alfonso@gmail.com>
2018-12-03 14:56:59 +0100

    Day02: Add failing test

---
+++ AdventOfCode2018.Tests/Day02_Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2018.Tests
+{
+    [TestClass()]
+    public class Day02_Tests
+    {
+        [TestMethod()]
+        public void ResolveDay02__Test1()
+        {
+            Day02 day02 = new Day02();
+
+            string result = day02.ResolveDay02(new string[] {
+                "abcdef",
+                "bababc",
+                "abbcde",
+                "abcccd",
+                "aabcdd",
+                "abcdee",
+                "ababab",
+            });
+
+            Assert.AreEqual("12", result);
+        }
+    }
+}
