a56f250b82976bf5313d37d85a6340b0cba9c38f
Valeriano A.R <valen.alfonso@gmail.com>
2018-12-03 14:02:41 +0100

    Day01: Add input data and resolve

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace AdventOfCode2018
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+
+            // Resolve Day01 input data
+            Day01 day01 = new Day01();
+            string[] lines = File.ReadAllLines("inputs/Day01.txt");
+            string result = day01.ResolveDay01(lines);
+            Console.WriteLine("Day01 Result: {0}", result);
+
+            Console.Read();
         }
     }
 }
