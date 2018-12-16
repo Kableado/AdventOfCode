6128917e02b3732b8d964b9ea24cd7b0fbdc639f
Valeriano A.R <valen.alfonso@gmail.com>
2018-12-16 14:25:03 +0100

    Day 13

---
+++ AdventOfCode2018.Tests/Day13_Tests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2018.Tests
+{
+    [TestClass()]
+    public class Day13_Tests
+    {
+        [TestMethod()]
+        public void ResolvePart1__Test1()
+        {
+            Day13 day = new Day13();
+
+            string result = day.ResolvePart1(new string[] {
+                "|",
+                "v",
+                "|",
+                "|",
+                "|",
+                "^",
+                "|",
+            });
+
+            Assert.AreEqual("0,3", result);
+        }
+
+        [TestMethod()]
+        public void ResolvePart1__Test2()
+        {
+            Day13 day = new Day13();
+
+            string result = day.ResolvePart1(new string[] {
+                @"/->-\        ",
+                @"|   |  /----\",
+                @"| /-+--+-\  |",
+                @"| | |  | v  |",
+                @"\-+-/  \-+--/",
+                @"  \------/   ",
+            });
+
+            Assert.AreEqual("7,3", result);
+        }
+
+        [TestMethod()]
+        public void ResolvePart2__Test()
+        {
+            Day13 day = new Day13();
+
+            string result = day.ResolvePart2(new string[] {
+                @"/>-<\  ",
+                @"|   |  ",
+                @"| /<+-\",
+                @"| | | v",
+                @"\>+</ |",
+                @"  |   ^",
+                @"  \<->/",
+            });
+
+            Assert.AreEqual("6,4", result);
+        }
+    }
+}
