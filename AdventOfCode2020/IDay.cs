8863f62fa81992915e10577751e6a7ab8f5a7c2c
Valeriano A.R <valen.alfonso@gmail.com>
2020-12-02 00:58:54 +0100

    Project files and Day01.

---
+++ AdventOfCode2020/IDay.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2020
+{
+    public interface IDay
+    {
+        string ResolvePart1(string[] inputs);
+        string ResolvePart2(string[] inputs);
+    }
+}
---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentDayNumber = 1;
+            IDay currentDay = null;
+
+            switch (currentDayNumber)
+            {
+                case 1: currentDay = new Day01(); break;
+            }
+
+            Console.WriteLine(string.Format("Day {0:00}", currentDayNumber));
+            Console.WriteLine("------");
+            Console.WriteLine();
+            string[] linesDay = File.ReadAllLines(string.Format("inputs/Day{0:00}.txt", currentDayNumber));
+            try
+            {
+                string resultPart1 = currentDay.ResolvePart1(linesDay);
+                Console.WriteLine("Day{1:00} Result Part1: {0}", resultPart1, currentDayNumber);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            try
+            {
+                string resultPart2 = currentDay.ResolvePart2(linesDay);
+                Console.WriteLine("Day{1:00} Result Part2: {0}", resultPart2, currentDayNumber);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            Console.Read();
+        }
+    }
+}
