ad5825a5ee145b9e393a9348503cfaa3e93797a8
Valeriano A.R <valen.alfonso@protonmail.com>
2023-04-09 23:32:15 +0200

    WIP

---
+++ AdventOfCode2020.Tests/Day10_Tests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestClass()]
+    public class Day10_Tests
+    {
+        [TestMethod()]
+        public void ResolvePart1__Example()
+        {
+            var day = new Day10();
+
+            string result = day.ResolvePart1(new string[] {
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+            });
+
+            Assert.AreEqual("YYY", result);
+        }
+
+        [TestMethod()]
+        public void ResolvePart2__Example()
+        {
+            var day = new Day09();
+
+            string result = day.ResolvePart2(new string[] {
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+                "XXXXXXXXXXXXXXX",
+            });
+
+            Assert.AreEqual("YYY", result);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day11.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+/*
+
+
+
+*/
+
+namespace AdventOfCode2020
+{
+    public class Day11 : IDay
+    {
+        public string ResolvePart1(string[] inputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ResolvePart2(string[] inputs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
